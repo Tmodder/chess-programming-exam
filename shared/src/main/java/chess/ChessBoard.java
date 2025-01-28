7d6bdd81d4dd25bc1dcfb0e0b7fa3b5a1944d0b6
Thomas Esplin <tom.esplin@gmail.com>
2025-01-28 09:38:38 -0700

    Add chessboard reset board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.Arrays;
 import java.util.Objects;
 
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -54,11 +55,66 @@ public class ChessBoard {
         return board[row][col];
     }
 
+    public void loadBoard(String boardText)
+    {
+        char[] chars = boardText.toCharArray();
+        int row = 0;
+        int col = 0;
+        for (char c : chars)
+        {
+            switch(c)
+            {
+                case '~':
+                    col++;
+                case ' ':
+                    break;
+                case '\n':
+                    row++;
+                    col = 0;
+                    break;
+                default:
+                    var color = Character.isLowerCase(c) ? ChessGame.TeamColor.BLACK : ChessGame.TeamColor.WHITE;
+                    c = Character.toLowerCase(c);
+                    var type = charToPiece(c);
+                    var piece = new ChessPiece(color, type);
+                    board[row][col] = piece;
+                    col++;
+                    break;
+            }
+        }
+    }
+
+    public ChessPiece.PieceType charToPiece(char c)
+    {
+        return switch(c)
+        {
+            case 'k' -> ChessPiece.PieceType.KING;
+            case 'q' -> ChessPiece.PieceType.QUEEN;
+            case 'b' -> ChessPiece.PieceType.BISHOP;
+            case 'r' -> ChessPiece.PieceType.ROOK;
+            case 'n' -> ChessPiece.PieceType.KNIGHT;
+            case 'p' -> ChessPiece.PieceType.PAWN;
+            default -> null;
+        };
+    }
+
+
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        String initBoard = """
+                r n b q k b n r
+                p p p p p p p p
+                ~ ~ ~ ~ ~ ~ ~ ~
+                ~ ~ ~ ~ ~ ~ ~ ~
+                ~ ~ ~ ~ ~ ~ ~ ~
+                ~ ~ ~ ~ ~ ~ ~ ~
+                P P P P P P P P
+                R N B Q K B N R
+                """;
+        loadBoard(initBoard);
     }
 }
